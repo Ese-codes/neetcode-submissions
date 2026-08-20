b8944d6d001e5860fcff3fba8b872985736bb28e
Alexia Okeregbe <71382127+Ese-codes@users.noreply.github.com>
2026-08-20 17:08:37 +0100

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pair = []
+        for i in range(len(nums) - 1):
+            changedNums = nums.copy()
+            second = target - nums[i]
+            changedNums.pop(i)
+            if second in changedNums:
+                pair = [i, changedNums.index(second)+1]
+                break
+        return pair
+            
